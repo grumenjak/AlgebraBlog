a2e7d18ace35520d8df70c255cd46665ce56a1f1
programer10 <programer10@ucione.local>
2019-07-23 21:14:47 +0200

    tags2

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,14 +13,18 @@
         {{ $post->created_at->toFormattedDateString() }} by<a href="#">{{ $post->user->name }}</a>
     </p>
     
+    @if (count($post->tags))
+        
     <section class="mb-3">
             <h6 class="d-inline">Tags:</h6>
             @foreach ($post->tags as $tag)
-                <a href="" $tag->name </a>
+                <a href="" class="badge badge-primary"> {{ $tag->name }} </a>
             @endforeach
 
     </section>
-    
+
+    @endif
+  
     <article class="text-justify">
         {{ $post->body }}
     </article>
